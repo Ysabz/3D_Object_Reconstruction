0cadb856a01c0d0bb547393ffbdbc033b2cbae6e
FrancisPicard2000 <francispicar72@gmail.com>
2024-11-11 13:45:33 -0500

    Moved data, renamed scripts and made them more interactable

--- RGBD_to_pcd.py
+++ RGBD_to_pcd.py
@@ -2,6 +2,7 @@ import open3d as o3d
 import numpy as np
 import cv2
 import os
+import argparse
 
 
 # Intel RealSense D415
@@ -10,25 +11,20 @@ focal_length = 597.522  ## mm
 img_center_x = 312.885
 img_center_y = 239.870
 
-def make_point_cloud(i):
+
+# This function creates a point cloud for a given frame
+def make_point_cloud(i, obj, depth_thresholds, width_range, height_range):
+
     # Read the images
-    img = cv2.imread(f'./train/spyderman2/rgb/align_test{i}.png')
+    img = cv2.imread(f'./train/{obj}/rgb/align_test{i}.png')
     img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-    depth = o3d.io.read_image(f'./train/spyderman2/depth/align_test_depth{i}.png')
+    depth = o3d.io.read_image(f'./train/{obj}/depth/align_test_depth{i}.png')
     depth = np.asarray(depth, np.float32)
 
-    # Remove the image points with a depth further than a certain value
-    # (belongs to the background)
-    threshold = 700 # 500
-    idx = np.where(depth > threshold)
+    # Filter image points based on depth
+    idx = np.where((depth < depth_thresholds[0]) or (depth > depth_thresholds[1]))
     depth[idx] = 0
-
     
-    threshold2 = 500
-    idx2 = np.where(depth < threshold2)
-    depth[idx2] = 0
-    
-
     # Create the normalized point cloud
     original_pcd = o3d.geometry.PointCloud()
     original_pcd_pos = []
@@ -37,17 +33,13 @@ def make_point_cloud(i):
     for v in range(img.shape[0]): # height
         for u in range(img.shape[1]): # width
             
-            
-            if (u > 460) or (u < 200):
+            # If the point is not in the cropped region, do not include it
+            if (u < width_range[0]) or (u > width_range[1]):
                 continue
-            
-            
-            '''
-            if (v > 420): # 405
-                continue
-            '''
-            
 
+            if (v < height_range[0]) or (v > height_range[1]):
+                continue
+            
             # Normalized image plane -> (u, v, 1) * z = zu, zv, z
             z = depth[v][u] / depth_scaling_factor # mm
             x = (u - img_center_x) * z / focal_length
@@ -63,7 +55,6 @@ def make_point_cloud(i):
     original_pcd.points = o3d.utility.Vector3dVector(original_pcd_pos)
     original_pcd.colors = o3d.utility.Vector3dVector(original_pcd_color)
 
-
     # Save point cloud
     o3d.io.write_point_cloud(f'./spyderman/spyderman{i}.pcd', original_pcd)
     #o3d.visualization.draw_geometries([original_pcd])
@@ -71,12 +62,34 @@ def make_point_cloud(i):
 
 
 if __name__ == "__main__":
-    os.makedirs('./spyderman', exist_ok=True)
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-ob", help="Object Name (castard, box, spyderman)", required=True)
+
+    # Used to filter the points based on depth
+    parser.add_argument("-mid", help="Minimum Depth (Castard: ?, Box: ?, Spyderman: 500)", required=True)
+    parser.add_argument("-mad", help="Maximum Depth (Castard: 500, Box: 500, Spyderman: 700)", required=True)
+
+    # Used to crop the object within the image
+    # IS THE IMAGE REVERSED?
+    parser.add_argument("-miw", help="Minimum width (Castard: ?, Box: ?, Spyderman: 200)", required=True)
+    parser.add_argument("-maw", help="Maximum width (Castard: ?, Box: ?, Spyderman: 460)", required=True)
+    parser.add_argument("-mih", help="Minimum height (Castard: ?, Box: ?, Spyderman: ?)", required=True)
+    parser.add_argument("-mah", help="Maximum height (Castard: ?, Box: ?, Spyderman: 420)", required=True)
+
+    args = parser.parse_args()
+    obj = args.ob
+    min_depth = args.mid
+    max_depth = args.mad
+    min_width = args.miw
+    max_width = args.maw
+    min_height = args.mih
+    max_height = args.mah
+
+    os.makedirs(f'./{obj}', exist_ok=True)
     #make_point_cloud(1)
     
-    
     for i in range(1, 23):
-        make_point_cloud(i)
+        make_point_cloud(i, obj, [min_depth, max_depth], [min_width, max_width], [min_height, max_height])
     
     
     
